7c8c2c4123d76896844eac9376ccf22f5cb1d251
choukseyabhishek <40685304+choukseyabhishek@users.noreply.github.com>
2018-07-12 08:56:08 +0000

    Update recipe.py

--- recipe.py
+++ recipe.py
@@ -3,6 +3,7 @@ import re
 R = re.compile("PSL")
 S1 = "PSLaaaaa Welcome to aaaaa.PSL persistent......"
 S2 = "aaaa PSL Welcome to aaaaa.persistent......."
+print("hii")
 if R.match(S1):
     print("Match Found")
 else:
